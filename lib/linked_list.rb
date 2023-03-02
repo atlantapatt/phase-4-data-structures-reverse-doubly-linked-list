85446915dcd66747c908ddbde8f61ceae135e2cf
AtlantaPatterson <atlantarpatterson@gmail.com>
2023-03-02 15:01:59 -0800

    done

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -9,6 +9,31 @@ class LinkedList
   end
 
   def reverse!
-    # your code here
+    return head if head == nil || head.next_node == nil
+    current_node = head
+
+    current = @head
+    @head = @tail
+    @tail = current
+
+    head = reverse_node(current)
+  end
+
+
+  def reverse_node(node)
+    if (node == nil)
+      return nil
+    end
+
+    temp_next = node.next_node
+
+    node.next_node = node.prev_node
+    node.prev_node = temp_next
+
+    if (node.prev_node == nil)
+      return node
+    end
+
+    return reverse_node(node.prev_node)
   end
 end
